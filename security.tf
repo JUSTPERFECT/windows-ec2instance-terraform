7cbd6f3cf9bb323e724dc76673a8c8994aeb8065
jayaprakash reddy <jayaprakash.jp63@gmail.com>
2017-07-05 00:24:31 +0530

    sg

---
+++ security.tf
@@ -0,0 +1,18 @@
+resource "aws_security_group" "default" {
+  name        = "terraform_example"
+  description = "Used in the terraform"
+
+  ingress {
+    from_port   = 5985
+    to_port     = 5986
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
