a21037528f7fa658fc6f3b57d5f239dee5ce8190
jayaprakash reddy <jayaprakash.jp63@gmail.com>
2017-07-05 00:08:08 +0530

    role creation

--- instance.tf
+++ instance.tf
@@ -1,8 +1,6 @@
-# Lookup the correct AMI based on the region specified
 data "aws_ami" "amazon_windows_2012R2" {
   most_recent = true
   owners      = ["amazon"]
-
   filter {
     name   = "name"
     values = ["Windows_Server-2012-R2_RTM-English-64Bit-Base-*"]
@@ -10,30 +8,17 @@ data "aws_ami" "amazon_windows_2012R2" {
 }
 
 resource "aws_instance" "winrm" {
-  # The connection block tells our provisioner how to
-  # communicate with the resource (instance)
   connection {
     type     = "winrm"
     user     = "Administrator"
     password = "${var.admin_password}"
-    # set from default of 5m to 10m to avoid winrm timeout
     timeout  = "30m"
   }
 
   instance_type = "t2.micro"
   ami           = "${data.aws_ami.amazon_windows_2012R2.image_id}"
-iam_instance_profile="kingkongrole"
-  # The name of our SSH keypair you've created and downloaded
-  # from the AWS console.
-  #
-  # https://console.aws.amazon.com/ec2/v2/home?region=us-west-2#KeyPairs
-  #
+  iam_instance_profile="kingkongrole"
   key_name = "${var.key_name}"
-
-  # Our Security group to allow WinRM access
-
-  # Note that terraform uses Go WinRM which doesn't support https at this time. If server is not on a private network,
-  # recommend bootstraping Chef via user_data.  See asg_user_data.tpl for an example on how to do that.
   user_data = <<EOF
 <script>
   winrm quickconfig -q & winrm set winrm/config @{MaxTimeoutms="1800000"} & winrm set winrm/config/service @{AllowUnencrypted="true"} & winrm set winrm/config/service/auth @{Basic="true"}
